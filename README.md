e7d707c1efb2c66802fa546d41cce50f36363363
Robin Percy <robin.percy@auth0.com>
2017-11-20 13:01:57 +0000

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+okta_erl_sample
+=====
+
+Baseline / Barebone Erlang application utilising Okta and OTP.
+
+This application with utilise a fully-supervised architecture and demonstrate Okta's User Management API, in a Erlang environment.
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/okta_erl_sample_sup.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @author rbnpercy
+%%% @copyright (C) 2017, Robin Percy
+%% @doc
+%%   okta_erl_sample top level supervisor.
+%% @end
+%%% Created : 20. Nov 2017
+%%%-------------------------------------------------------------------
+-module(okta_erl_sample_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    {ok, { {one_for_all, 0, 1}, []} }.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
